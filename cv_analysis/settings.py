d2b7632f5dae67264620c180156c5114ffa15de7
ow-tech <wagwanwekon@outlook.com>
2025-02-18 01:50:44 +0400

    created a basic home view and template, configured url for home view and added app in project installed apps

--- cv_analysis/settings.py
+++ cv_analysis/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'cv_tracking_system.apps.CvTrackingSystemConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
