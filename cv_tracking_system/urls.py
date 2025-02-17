60dde89790a82708dfa898c08cec108d2098a550
ow-tech <wagwanwekon@outlook.com>
2025-02-18 02:37:25 +0400

    created base template which has Nav, footer, body clickable anchor tags, this has been implemented in home html

--- cv_tracking_system/urls.py
+++ cv_tracking_system/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+      path('upload/', views.UploadView.as_view(), name='upload'),
+    path('candidates/', views.CandidatesView.as_view(), name='candidates'),
+    path('candidates/<int:pk>/', views.CandidateDetailView.as_view(), name='candidate_detail'),
+    path('chat/', views.ChatView.as_view(), name='chat'),
    
 ]
--- cv_tracking_system/views.py
+++ cv_tracking_system/views.py
@@ -4,3 +4,16 @@ from django.views import View
 class HomeView(View):
     def get(self, request):
         return render(request, 'cv_tracking_system/home.html')
+class UploadView(View):
+    def get(self, request):
+        return render(request, 'cv_tracking_system/home.html')
+class CandidatesView(View):
+    def get(self, request):
+        return render(request, 'cv_tracking_system/home.html')
+
+class CandidateDetailView(View):
+    def get(self, request, pk):
+        return render(request, 'cv_tracking_system/home.html')
+class ChatView(View):
+    def get(self, request, pk):
+        return render(request, 'cv_tracking_system/home.html')
